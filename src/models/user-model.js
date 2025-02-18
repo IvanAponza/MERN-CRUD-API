df6f2168adee16dba9a22ba0d8adba8b3f6afcda
ivan <aponzacantoni@gmail.com>
2025-02-17 21:29:16 -0500

    feat: Se agrega modelo usuario

---
+++ src/models/user-model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true, 'name is required'],
+        trim: true,
+    },
+    email:{
+        type:String,
+        required:[true, 'email is required'],
+        unique:true,
+        trim: true,
+    },
+    password:{
+        type:String,
+        required:[true, 'password is required'],
+        unique:true,
+    },
+    roles:{
+        type:[String],
+        default: ['USER_ROLE'],
+        enum: ['ADMIN_ROL', 'USER_ROLE'],
+        trim: true,
+    },
+    img: {
+        type: String
+    }
+});
+
+export default mongoose.model('User', userSchema);
