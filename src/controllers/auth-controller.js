adcc1d0856370099222e479778e76f6328f30e8b
ivan <aponzacantoni@gmail.com>
2025-02-17 23:05:00 -0500

    feat: crea funcion para adaptar bcrypt.js y se implementa la encryptacion de la passwor y se excluye de la respuesta

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,9 +1,9 @@
 import Users from '../models/user-model.js';
+import {bcryptAuth} from '../libs/bcrypt.js';
 
 export const register = async(req, res) => {
     const {username, email, password} = req.body;
 
-    
     try {
         const userFound = await Users.findOne({email});
         if(userFound) return res.status(400).json({message: 'User already exist'});
@@ -13,8 +13,19 @@ export const register = async(req, res) => {
             email,
             password,
         });
+        user.password = bcryptAuth.hash(password);
+
         const newUser = await user.save();
-        return res.status(200).json(newUser);
+
+        return res.status(200).json({
+            id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            roles: newUser.roles,
+            img: newUser.img,
+            createdAt:newUser.createdAt,
+            updatedAt:newUser.updatedAt,
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: 'Internal Server Error'});
---
+++ src/libs/bcrypt.js
@@ -0,0 +1,13 @@
+import bcrypt, { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+
+export const bcryptAuth = {
+
+    hash: (password) => {
+        const salt = genSaltSync();
+        return hashSync(password, salt);
+    },
+
+    compare: (password, hashed) => {
+        return compareSync(password, hashed);
+    }
+}
