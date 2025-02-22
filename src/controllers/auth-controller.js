a14c31783325ded78837b2aa9aac556a7cd412f5
ivan <aponzacantoni@gmail.com>
2025-02-21 22:36:24 -0500

    feat: se crean empoint y cotroladores para eliminar, perfil, actualizar, eliminar, se crea ruta protegida

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -62,6 +62,62 @@ export const login = async(req, res) => {
     }
 }
 
+export const getUsers = async(req, res) => {
+    try {
+        const users = await Users.find({});
+        return res.status(200).json({users});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: 'Internal Server Error'});
+    }
+}
+export const perfil = async(req, res) => {
+    try {
+        const userFound = await Users.findById(req.user.id);
+        if(!userFound) return res.status(400).json({message: 'User not found'});
+        return res.status(200).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            roles: userFound.roles,
+            img: userFound.img,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({message: 'Internal Server Error'});
+    }
+}
+export const updateUser = async(req, res) => {
+    try {
+        const userFound = await Users.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!userFound) return res.status(400).json({message: 'User not found'});
+        return res.status(200).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            roles: userFound.roles,
+            img: userFound.img,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({message: 'Internal Server Error'});
+    }
+}
+export const deleteUser = async(req, res) => {
+    try{
+        const userFound = await Users.findByIdAndDelete(req.params.id);
+        if(!userFound) return res.status(400).json({message: 'User not found'});
+        return res.status(200).json({message: 'User deleted successfully!'});
+    } catch(error){
+        console.log(error);
+        return res.status(400).json({message: 'Internal Server Error'});
+    }
+}
+
 export const logout = async(req, res) => {
     res.cookie('token', "", {expires: new Date(0)});
     return res.sendStatus(200);
--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,12 +1,19 @@
 import { Router } from 'express';
-import { login, logout, register } from '../controllers/auth-controller.js';
+import { deleteUser, getUsers, login, logout, perfil, register, updateUser } from '../controllers/auth-controller.js';
 import {validateSchema} from '../middleware/validate.middleware.js';
 import {loginSchema, registerSchema} from '../schema/auth.schema.js';
+import { authRequired } from '../middleware/validateToken.js';
 
 const router = Router();
 
 router.post('/register', validateSchema(registerSchema), register);
 router.post('/login', validateSchema(loginSchema), login);
+router.get('/users', getUsers);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 router.post('/logout', logout);
+
+//Protected
+router.get('/perfil', authRequired, perfil);
 
 export default router;
